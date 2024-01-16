b86d77298f3390d7caef8c361c7dc93b9259ec0e
TimLinCa <TimLin626@gmail.com>
2024-01-15 17:17:51 -0700

    Create Week2

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>HuiEn Lin</h1>
+            <Link href="https://github.com/TimLinCa">TimLinCa</Link>
+        </div>
+    );
+};
+
